688d428ff579a9990a255e70d99ffaf00688ad33
robertlongmendez <robertlongmendez@gmail.com>
2021-07-26 10:20:04 -0500

    Changed navbar theme

--- src/app/movie-feed/movie-feed.component.ts
+++ src/app/movie-feed/movie-feed.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { HttpClient } from '@angular/common/http';
 import { Post } from "../shared/poster.model";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-movie-feed',
