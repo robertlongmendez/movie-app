d6084419b9a6b40ba69b9a0929a3c53840c978a0
robertlongmendez <82781708+robertlongmendez@users.noreply.github.com>
2021-07-30 13:48:03 -0500

    Update movie.service.ts

--- src/app/movies/movie.service.ts
+++ src/app/movies/movie.service.ts
@@ -9,23 +9,20 @@ export class MovieService {
 
   // private movies: Movie[] = [
   //   new Movie(
-  //     'Attack of the Killer Tomatoes',
+  //      'Attack of the Killer Tomatoes',
   //     'Aaargh!...',
   //     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrvQEBxBu-xAvpeHP6Srio3VI2iXj26vLC5BnM8qGETOezQ4ergsY60jlpWpn56XI8sTQ&usqp=CAU',
-  //       new Facts('Attack of the Killer Tomatoes', 'Horror Comedy', '1978')
-  //     ),
+  //     '1989'),
   //   new Movie(
   //     'Arcade',
   //     'The game wants to play with you.',
   //     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6nUdtA3UVAPClm30Jz2TPzNVrijb6wrpiF0ciY5NJ3QEqbwmPcwRma0_xzBTpxIzBOiY&usqp=CAU',
-  //     new Facts('Arcarde', 'Sci-fi Horror', '1993')
-  //   ),
+  //     '1989'),
   //   new Movie(
   //     'The Giant Gila Monster',
   //     'Pretty Straight Forward',
   //     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwLsLW26rNqWteyo7fIdRfB7-IuGvzXbLnF1zkQ5S1IcDoDrB2HWgyHedrah7vYHqWsQ8&usqp=CAU',
-  //     new Facts('The Giant Gila Monster', 'Sci-fi Horror', '1959')
-  //   )
+  //     '1989' )
   // ];
   private movies: Movie[] = [];
 
