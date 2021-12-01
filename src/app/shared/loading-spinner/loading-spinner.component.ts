cefff5918e7ec7f4e42afeae14ef0e3e832f0f2d
robertlongmendez <82781708+robertlongmendez@users.noreply.github.com>
2021-11-30 19:13:10 -0600

    loading spinner added

--- src/app/shared/loading-spinner/loading-spinner.component.ts
+++ src/app/shared/loading-spinner/loading-spinner.component.ts
@@ -2,9 +2,10 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-loading-spinner',
-  template: '<div class="loadingio-spinner-bean-eater-2v5vxj86qv3"><div class="ldio-qqmbz3ok1ys"><div><div></div><div></div><div></div></div><div><div></div><div></div><div></div></div></div></div>',
+  template: '<div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>',
   styleUrls: ['./loading-spinner.component.css']
 })
 
 export class LoadingSpinnerComponent {}
 
+// <div class="loadingio-spinner-bean-eater-2v5vxj86qv3"><div class="ldio-qqmbz3ok1ys"><div><div></div><div></div><div></div></div><div><div></div><div></div><div></div></div></div></div>
